334b658f8cbb13b03e70f735dbb735d26dce057a
CJLorax <casey.jones@tstc.edu>
2019-03-10 18:05:29 -0500

    Added drawing red placeholder image mac
    Added drawing red placeholder image mac

--- source/main.cpp
+++ source/main.cpp
@@ -12,6 +12,8 @@
 	#include <SDL2_net/SDL_net.h>
 	#include <SDL2_mixer/SDL_mixer.h>
 	#include <SDL2_ttf/SDL_ttf.h>
+
+	#include <unistd.h>
 #endif
 
 #if defined(__linux__)
@@ -31,11 +33,18 @@
 #include <iostream>
 using namespace std;
 
+
 int main(int argc, char* argv[]) {
 
 
 #if defined(__APPLE__)
 	cout << "Running on OS X..." << endl;
+
+	// get current working directory and append the images folder to it
+	string s_cwd(getcwd(NULL,0));
+	string s_cwd_images = s_cwd + "/Assets/images";
+
+	cout << s_cwd_images << endl;
 #endif
 
 #if defined(__linux__)
@@ -51,6 +60,9 @@ int main(int argc, char* argv[]) {
 
 
     SDL_Window *window;                    // Declare a pointer
+
+    // create a renderer variable
+    SDL_Renderer *renderer = NULL;
 
     SDL_Init(SDL_INIT_EVERYTHING);              // Initialize SDL2
 
@@ -71,6 +83,39 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // create renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // create a path to the background image
+    string BKGDpath = s_cwd_images + "/placeholder.png";
+
+    // create a SDL surface
+    SDL_Surface *surface = IMG_Load(BKGDpath.c_str());
+
+    // create texture
+    SDL_Texture *bkgd1;
+
+    // place surface into the texture
+    bkgd1 = SDL_CreateTextureFromSurface(renderer, surface);
+
+    // create the rectangle for the texture
+    SDL_Rect bkgd1Pos;
+
+	bkgd1Pos.x = 0;
+	bkgd1Pos.y = 0;
+	bkgd1Pos.w = 256;
+	bkgd1Pos.h = 256;
+
+
+	// draw the screen
+	//clear the old buffer
+	SDL_RenderClear(renderer);
+
+	SDL_RenderCopy(renderer, bkgd1, NULL, &bkgd1Pos);
+
+	// draw new info to the screen
+	SDL_RenderPresent(renderer);
+
 
     bool isquit = false;
     SDL_Event event;
@@ -80,6 +125,8 @@ int main(int argc, char* argv[]) {
                 isquit = true;
             }
         }
+
+
     }
 
     // Close and destroy the window
