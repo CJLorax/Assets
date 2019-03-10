fe17896ee916c73f358a80ce58569e92a2f2e2b9
CJLorax <casey.jones@tstc.edu>
2019-03-10 15:55:52 -0500

    Commit from Linux #1
    Added reposiesitory to the Ubunut project and linked remaining librar

--- source/main.cpp
+++ source/main.cpp
@@ -16,6 +16,10 @@
 
 #if defined(__linux__)
 	#include <SDL2/SDL.h>
+	#include <SDL2/SDL_image.h>
+	#include <SDL2/SDL_net.h>
+	#include <SDL2/SDL_mixer.h>
+	#include <SDL2/SDL_ttf.h>
 #endif
 
 #if defined(_WIN32) || (_WIN64)
